33310139a5098234dd89ae76cffcfb6db32d9639
Duong1710 <tuthu999777@gmail.com>
2025-03-13 23:28:02 +0700

    Tạo API để lấy thông tin người dùng qua Postman + Xampp chưa database

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
-import homeController, { getHomePage } from "../controllers/homeController";
+import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 //express.Router() tạo một bộ định tuyến để quản lý các tuyến đường.
@@ -13,6 +14,11 @@ let initWebRoutes = (app) => {
     router.get("/edit-crud", homeController.getEditCRUD);
     router.post("/put-crud", homeController.putCRUD);
     router.get("/delete-crud", homeController.deleteCRUD);
+
+    // Tương tác với frontend
+    router.post("/api/login", userController.handleLogin)
+
+
     return app.use("/", router); // Gắn tất cả các route vào ứng dụng Express.
 }
 
---
+++ src/services/userService.js
@@ -0,0 +1,74 @@
+
+import db from "../models/index.js";
+import bcrypt from "bcryptjs"; // import thư viện
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExit = await checkUserEmail(email);
+            if (isExit) {
+                // user existed
+                let user = await db.User.findOne({
+                    where: { email: email }, // email trong db bằng email truyền vào 
+                    attributes: ['email', 'roleId', 'password'],
+                    /*
+                    mục đích là in ra 2 thuộc tính email và roleId nhưng cũng phải lấy cả password nhằm mục 
+                    đích lát phải check password ở dưới. Sau đó có dòng delete user.password nhằm tránh lộ password ra ngoài
+                     */
+                    raw: true
+                });
+                if (user) { // user có tồn tại
+                    // check password
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = `Okeee`;
+                        console.log(user);
+                        delete user.password; // Để không lộ password ra - yêu cầu phải để raw: true để trả về user là 1 object -> lúc ấy mới delete kiểu object được
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = `Wrong password`;
+                    }
+                }
+                else {
+                    userData.errCode = 2;
+                    userData.errMessage = "User is not exist"
+                }
+
+            }
+            else {
+                userData.errCode = 1;
+                userData.errMessage = `Your's Email is not exist`
+            }
+            resolve(userData)
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail } // email trong db bằng email truyền vào thì tìm được
+            })
+            if (user) {
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+
+
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+
+}
